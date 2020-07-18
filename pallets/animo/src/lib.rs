8900eb7051c9b47351b2bd2d1bb62d6998cca99e
Dmitriy Shabanov <shabanovd@gmail.com>
2020-07-18 21:33:12 +0300

    Storage modifications prototype

--- pallets/animo/src/lib.rs
+++ pallets/animo/src/lib.rs
@@ -1,16 +1,20 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// A FRAME pallet template with necessary imports
-
-/// Feel free to remove or edit this file as needed.
-/// If you change the name of this file, make sure to update its references in runtime/src/lib.rs
-/// If you remove this file, you can remove those references
-
-/// For more guidance on Substrate FRAME, see the example pallet
-/// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
-
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{
+	decl_module, decl_storage, decl_event, decl_error,
+	dispatch::{DispatchResult, Vec},
+	ensure
+};
 use frame_system::{self as system, ensure_signed};
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use codec::{Decode, Encode};
+
+use sp_core::H256;
+use sp_runtime::{
+	transaction_validity::{TransactionLongevity, ValidTransaction},
+};
 
 #[cfg(test)]
 mod mock;
@@ -26,26 +30,46 @@ pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
+/// Modification to be dispatched
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct Modification {
+	pub changes: Vec<Change>,
+}
+
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct Change {
+	/// primary object of relation
+	pub primary: ID,
+
+	/// description of relation between primary object and value
+	pub relation: IDS,
+
+	/// value before modification
+	pub before: Value,
+
+	/// value after modification
+	pub after: Value,
+}
+
+pub type ID = H256;
+pub type IDS = Vec<ID>;
+pub type Value = Vec<u8>;
+
+
 // This pallet's storage items.
 decl_storage! {
-	// It is important to update your storage name so that your pallet's
-	// storage items are isolated from other pallets.
-	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
+	trait Store for Module<T: Trait> as AnimoModule {
+		AnimoStore get(fn animo_store): double_map hasher(blake2_128_concat) ID, hasher(blake2_128_concat) IDS => Option<Value>
 	}
 }
 
 // The pallet's events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
+		/// Modification was applied
+		ModificationAccepted(Modification, AccountId),
 	}
 );
 
@@ -72,38 +96,52 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		#[weight = 10_000]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
+		/// Dispatch a single modification
+		#[weight = 1_000_000]
+		pub fn modify(origin, modification: Modification) -> DispatchResult {
+			// Check it was signed and get the signer.
 			let who = ensure_signed(origin)?;
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+			let validity = Self::validate_modification(&modification)?;
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// TODO check permissions
+
+			Self::update_storage(&modification, validity.priority)?;
+
+			Self::deposit_event(RawEvent::ModificationAccepted(modification, who));
+
 			Ok(())
 		}
+	}
+}
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		#[weight = 10_000]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
+// "Internal" function, callable by code
+impl<T: Trait> Module<T> {
 
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
+	pub fn validate_modification(modification: &Modification) -> Result<ValidTransaction, &'static str> {
+		for change in modification.changes.iter() {
+			ensure!(!change.relation.is_empty(), "no relation description");
+			ensure!(!change.before.is_empty(), "no before state");
+			ensure!(!change.after.is_empty(), "no after state");
 		}
+
+		Ok(ValidTransaction {
+			requires: Vec::new(),
+			provides: Vec::new(),
+			priority: 1 as u64,
+			longevity: TransactionLongevity::max_value(),
+			propagate: true
+		})
+	}
+
+	fn update_storage(modification: &Modification, _reward: u64) -> DispatchResult {
+		for change in modification.changes.iter() {
+			let mut relation = change.relation.clone();
+			relation.sort();
+
+			<AnimoStore>::insert(change.primary, relation, change.after.clone());
+		}
+
+		Ok(())
 	}
 }
